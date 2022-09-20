7c6d24aceb1d7590a1e1c68b9f2e60e042812f3c
Artem <Styoganov@bk.ru>
2022-09-20 17:56:47 +0300

    Array and Check

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ArrayStrings = new string[] {"1234", "1", "1567", "-2", "computer science"};
+
+int length = 3; 
+int CheckArray(string[] array, int length)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= length) result++;
+    }
+    return result;
+}
+
